8c51d77112feaa6c5461c97bdd9ea4021a51e9aa
czavala <32529652+czavalap@users.noreply.github.com>
2025-02-24 18:47:23 -0500

    feat: enemigo3 animado, avances en script de daño

--- Assets/_ScriptsChristian/Explotar.cs
+++ Assets/_ScriptsChristian/Explotar.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Explotar : MonoBehaviour
 {
   
    [SerializeField] private Transform explosion;
+   [SerializeField] private float segundosInvencible = 4.0f;
+   private static bool invulnerable = false;
+   private Material matA, matB;
+
+   private Impacto ImpactoScript;
+
+   void Start()
+   {
+        ImpactoScript = GameObject.Find("megamanxCompleto").GetComponent<Impacto>();
+        matA = ImpactoScript.getMaterialA();
+        matB = ImpactoScript.getMaterialB();
+   }
 
    void OnTriggerEnter(Collider Col) 
    {
-        //if col es megaman o muro
+       if(Col.tag == "Player")
+        {
+            if(!invulnerable)
+            {
+                Explota();
+                Col.transform.parent.GetComponent<Animator>().SetTrigger("Damage");
+                invulnerable = true;
+                matA.SetFloat("_Invulnerable",1.0f);
+                matB.SetFloat("_Invulnerable",1.0f);
+                StartCoroutine("Invulnerable");
+                //Col.transform.parent.GetComponent<Animator>().SetBool("Damage", false); 
+            }
+            
+            
+        }
 
-        Explota();
-        //megaman.RecibeDaño
+        else
+            Explota();
+                
+   }
+
+   public IEnumerator Invulnerable()
+   {   
+        yield return new WaitForSeconds(segundosInvencible);
+        invulnerable = false;
+        matA.SetFloat("_Invulnerable",0.0f);
+        matB.SetFloat("_Invulnerable",0.0f);         
+        yield return null;
    }
 
   
--- Assets/_ScriptsChristian/Impacto.cs
+++ Assets/_ScriptsChristian/Impacto.cs
@@ -4,8 +4,13 @@ public class Impacto : MonoBehaviour
 {
     [SerializeField] private Material mat;
     [SerializeField] private Transform quadImpacto;
+    [SerializeField] private Transform muerte;
+    [SerializeField] private Material vidaUI;
+    [SerializeField] private Material megamanMatA;
+    [SerializeField] private Material megamanMatB;
     private Material controlImpacto;
     private Animator animator;
+    private float vida = 10f;
 
     void Start()
     {
@@ -13,13 +18,24 @@ public class Impacto : MonoBehaviour
             controlImpacto = quadImpacto.GetComponent<Renderer>().sharedMaterial;
         Reset();
         animator = GetComponent<Animator>();
+        vidaUI.SetFloat("_Salud", 1.0f);
     }
 
+    public Material getMaterialA()
+    {
+        return megamanMatA;
+    }
+
+    public Material getMaterialB()
+    {
+        return megamanMatB;
+    }
+/*
     void Update()
     {
         if (!animator.GetBool("Damage"))
             Reset();
-    }
+    }*/
 
     void Impactar()  // quedaria mejor con corutina?, cambio menos brusco pero afectaria sincronizacion con las transiciones de anims?
     {
@@ -27,7 +43,40 @@ public class Impacto : MonoBehaviour
         {
             mat.SetFloat("_Damage",1.0f);
             controlImpacto.SetFloat("_Impacto",1.0f);
+            megamanMatA.SetFloat("_Invulnerable",1.0f);
+            megamanMatB.SetFloat("_Invulnerable",1.0f);
+
+
+            //temporal
+            vida -= 3; 
+        if( vida <= 0)
+            Muerte();                  
+            
+        vidaUI.SetFloat("_Salud", vida/10f);
+        // temporal
+
         }
+    }
+
+    void Damage (float d)
+    {
+
+        vida -= d; 
+        if( vida <= 0)
+            Muerte();                  
+            
+        vidaUI.SetFloat("_Salud", vida/10f);
+
+    }
+
+
+    void Muerte()
+    {
+        muerte.gameObject.SetActive(true);
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(5).gameObject.SetActive(false); // collider ancho...
+        vidaUI.SetFloat("_Salud", 0.0f);
+        // UI game Over, en realidad pantalla se pone en blanco gradualmente y si fueron 3 veces sale ventana de password
     }
 
     void Reset()
@@ -35,7 +84,7 @@ public class Impacto : MonoBehaviour
         if(mat)
         {
             mat.SetFloat("_Damage",0.0f);
-           controlImpacto.SetFloat("_Impacto",0.0f);
+           controlImpacto.SetFloat("_Impacto",0.0f);            
         }
     }
    
